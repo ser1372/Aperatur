56517a5679b4ae8e61db1ba05b9b43aaad6cfb04
ser1372 <ser1372@ukr.net>
2022-11-08 15:50:11 +0200

    Scroll remove

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,13 @@
+let menuBtn = document.querySelector(".menu-btn");
+let menu = document.querySelector(".menu");
 
+menuBtn.addEventListener("click", function () {
+  menuBtn.classList.toggle("active");
+  menu.classList.toggle("active");
 
-let menuBtn = document.querySelector('.menu-btn');
-let menu = document.querySelector('.menu');
-menuBtn.addEventListener('click', function(){
-	menuBtn.classList.toggle('active');
-	menu.classList.toggle('active');
-})
-
-
+  if (menuBtn.classList.contains("active")) {
+    document.body.style.overflow = "hidden";
+  } else {
+    document.body.style.overflow = "visible";
+  }
+});
